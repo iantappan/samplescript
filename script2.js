e118c61378a33fde4590176a5e9d90460b486362
iantappan <142432751+iantappan@users.noreply.github.com>
2023-10-10 16:53:43 -0600

    Create script2.js

---
+++ script2.js
@@ -0,0 +1,7 @@
+<script>
+    document.addEventListener('DOMContentLoaded', function() {
+        if (document.querySelector('footer')) {
+            alert('Hello Ian's World!');
+        }
+    });
+</script>
